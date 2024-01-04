988c03f232edbee107a80172206de4561a29e7b5
Mohsin Ejaz <mejaz.bese21@gmail.com>
2024-01-04 13:48:24 -0800

    cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,9 @@
 import { store$ } from '@src/store'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function HomePage() {
-    const color = store$.color.use();
     return (
-        <>
-            {`Color :: ` + color}
-            <CompGreen />
-            <CompBlue />
-        </>
-    )
-}
-
-function CompGreen() {
-    const color$ = store$.color;
-    return (
-        <button onClick={() => color$.set('green')}>Green</button>
-    )
-}
-
-function CompBlue() {
-    const color$ = store$.color;
-    return (
-        <button onClick={() => color$.set('blue')}>Blue</button>
+       <p>Home</p>
     )
 }
 
